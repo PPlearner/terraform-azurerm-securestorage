64a47565f78445694000e9c55fbfa0864bde203a
Jayant Mathely <jayantravi@hotmail.com>
2026-01-08 18:57:19 +0000

    version 1

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+  type = string
+}
+variable "location" {
+  type = string
+
+}
+variable "name" {
+  type = string
+
+}
+variable "storage_account_name" {
+  type = string
+
+}
+variable "account_tier" {
+  type    = string
+  default = "Standard"
+
+}
+variable "account_replication_type" {
+  type    = string
+  default = "GRS"
+}
